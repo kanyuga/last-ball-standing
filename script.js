fc4540d10b294bff4d6c6492b500949445c96254
kanyuga <chriskanyuga@gmail.com>
2016-01-01 16:42:35 +0300

    Major js Changes: Added Cell objects to represent each cell Refactored Game object to use Cell Created cellClickHandler function from anonymous function
    Added fonts to repo

--- script.js
+++ script.js
@@ -1,78 +1,155 @@
+var gameArea = $('#game-area');
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+function Cell(row, col) {
+    this.row = row;
+    this.col = col;
+    this.element = null;
+
+    Cell.filledClass = 'filled';
+    Cell.selectedClass = 'selected';
+    Cell.itemClass = 'item';
+
+    this.fill = function() {
+        this.element.addClass(Cell.filledClass);
+        return this;
+    };
+
+    this.empty = function() {
+        this.element.removeClass(Cell.filledClass);
+        return this;
+    };
+
+    this.isFilled = function() {
+        return this.element.hasClass(Cell.filledClass);
+    };
+
+    this.select = function() {
+        this.element.addClass(Cell.selectedClass);
+        return this;
+    };
+
+    this.unselect = function() {
+        this.element.removeClass(Cell.selectedClass);
+        return this;
+    };
+
+    this.init = function() {
+        this.element = $('<div></div>')
+            .data({'row': this.row, 'col': this.col})
+            .attr({'id': 'item-' + this.row + '-' + this.col, 'class': Cell.itemClass + ' ' + Cell.filledClass});
+        gameArea.append(this.element);
+        return this;
+    };
 }
 
-function Game(gamearea, rows, cols) {
-    this.currentSpot = null;
-    this.gameArea = $(gamearea);
-    this.itemClass = "item";
-    this.selectedClass = "selected";
-    this.filledClass = "filled";
+function Game(rows, cols) {
     this.rows = rows;
     this.cols = cols;
-    
+    this.cells = [];
+    this.currentCell = null;
+
     this.init = function() {
-        var self = this;
-        self.currentSpot = null;
-        self.gameArea.html("");
-        for (i = 1; i <= self.rows; i++) {
-            for (j = 1; j <= self.cols; j++) {
-                self.gameArea.append("<div class='" + self.itemClass + " " + self.filledClass + "' data-row=" + i + " data-col=" + j + "></div>");
+        gameArea.html('');
+        for (var row = 1; row <= this.rows; row++) {
+            var r = [];
+            for (var col = 1; col <= this.cols; col++) {
+                r[col - 1] = new Cell(row, col).init();
             }
-            self.gameArea.append("<br/>");
+            this.cells[row - 1] = r;
+            gameArea.append('<br/>');
         }
-        self.getSpot(getRandomInt(1, rows + 1), getRandomInt(1, cols + 1)).removeClass(self.filledClass);
-        $("." + self.itemClass, self.gameArea).on("click", function() {
-            if ($(this).hasClass(self.filledClass)) {
-                if (!self.currentSpot) {
-                    self.currentSpot = $(this).addClass(self.selectedClass);
-                } else {
-                    self.currentSpot.removeClass(self.selectedClass);
-                    self.currentSpot = null;
+
+        this.getCell(Math.ceil(Math.random() * this.rows), Math.ceil(Math.random() * this.cols)).empty();
+
+        $("." + Cell.itemClass, gameArea).on('click', { 'game' : this}, cellClickHandler);
+    };
+
+    this.getCell = function(row, col) {
+        return this.cells[row - 1][col - 1];
+    };
+
+    this.filledCellsLeft = function () {
+        var total = 0;
+        for (row = 1; row <= this.rows; row++) {
+            for (col = 1; col <= this.cols; col++) {
+                if (this.getCell(row, col).isFilled()) {
+                    total++;
                 }
-            } else {
-                if (self.currentSpot) {
-                    //console.log("here");
-                    var rowDiff = Math.abs($(this).data("row") - self.currentSpot.data("row")),
-                        colDiff = Math.abs($(this).data("col") - self.currentSpot.data("col"));
-                    if ((colDiff === 0 || colDiff === 2) && (rowDiff === 0 || rowDiff === 2) && (colDiff === 2 || rowDiff === 2)) {
-                        var over = self.getSpot(($(this).data("row") + self.currentSpot.data("row")) / 2, 
-                                                ($(this).data("col") + self.currentSpot.data("col")) / 2);
-                        if (over.hasClass(self.filledClass)) {
-                            over.removeClass(self.filledClass);
-                            self.currentSpot.removeClass(self.selectedClass + " " + self.filledClass);
-                            self.currentSpot = null;
-                            $(this).addClass(self.filledClass);
-                            if ($("." + self.itemClass + "." + self.filledClass, self.gameArea).length === 1) {
-                                alert("You win!!");
-                            }
-                        }
+            }
+        }
+        return total;
+    };
+
+    this.hasMovesLeft = function () {
+        for (row = 1; row <= this.rows; row++) {
+            for (col = 1; col <= this.cols; col++) {
+                if (this.getCell(row, col).isFilled()) {
+                    if ((col > 1 && this.getCell(row, col - 1).isFilled()) // check left
+                        || (col < this.cols && this.getCell(row, col + 1).isFilled()) // check right
+                        || (row > 1 && this.getCell(row - 1, col).isFilled()) // check top
+                        || (row < this.rows && this.getCell(row + 1, col).isFilled())) { // check bottom
+                        return true;
                     }
                 }
             }
-        });
-    };
-    
-    this.getSpot = function(row, col) {
-        return $("." + this.itemClass + "[data-row=" + row + "][data-col=" + col + "]", this.gameArea);
-    };
-    
-    this.reset = function(rows, cols) {
-        console.log(this.rows, this.cols);
-        this.rows = rows;
-        this.cols = cols;
-        console.log(this.rows, this.cols);
-        this.init();
-    };
+        }
+        return false;
+    }
 }
 
+function cellClickHandler(event) {
+    game = event.data.game;
+    var cell = game.getCell($(this).data('row'), $(this).data('col'));
+
+    if (cell.isFilled()) {
+        if (!game.currentCell) {
+            cell.select();
+            game.currentCell = cell;
+        } else {
+            game.currentCell.unselect();
+            game.currentCell = null;
+        }
+    }
+    else if (game.currentCell) {
+        var rowDiff = Math.abs(game.currentCell.row - cell.row), colDiff = Math.abs(game.currentCell.col - cell.col);
+        if ((colDiff === 0 || colDiff === 2)
+            && (rowDiff === 0 || rowDiff === 2)
+            && ((colDiff === 2 && rowDiff === 0) || (colDiff === 0 && rowDiff === 2))) {
+            var over = game.getCell((game.currentCell.row + cell.row) / 2, (game.currentCell.col + cell.col) / 2);
+            if (over.isFilled()) {
+                cell.fill();
+                over.empty();
+                game.currentCell.empty().unselect();
+
+                if (game.filledCellsLeft() == 1) {
+                    alert(':) You Win!!');
+                }
+                else if (!game.hasMovesLeft()) {
+                    // You lose!
+                    alert(':( No moves left. You lose.');
+                }
+            }
+        }
+    }
+}
 
 $(function() {
-    game = new Game("#game-area", 4, 4);
+    var game = new Game(4, 4);
     game.init();
     $('#reset-form').on("submit", function(event) {
-        game.reset(parseInt($("#rows").val()), parseInt($("#cols").val()));
         event.preventDefault();
+        new Game(parseInt($("#rows").val()), parseInt($("#cols").val())).init();
+        $("#game-area").show();
+        $("#form, #how-to").hide();
+    });
+
+    $('#new-game').on('click', function() {
+        $("#form").show();
+        $("#game-area, #how-to").hide();
+    });
+
+    $('#rules').on('click', function() {
+        $("#how-to").show();
+        $("#game-area, #form").hide();
     });
 });
